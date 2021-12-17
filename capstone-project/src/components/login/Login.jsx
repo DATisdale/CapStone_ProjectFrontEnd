5f9d979a3292e2ca84c5d769dc9b3618834a98ab
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2021-12-17 17:23:41 -0500

    complete login page did not import to app file yet

---
+++ capstone-project/src/components/login/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import "./LoginForm.css";
+import axios from 'axios';
+import PropTypes from 'prop-types'
+import { Login } from '@mui/icons-material';
+
+function Login(props){
+    const[email,getEmail]=useState('');
+    const[password, getPassword]=useState('');
+
+
+async function handlesubmit(e){
+    e.preventDefault();
+
+
+const getUser={
+    email:email,
+    password:password
+}
+let response = await axios.post(`http://localhost:3000/api/users/login`, getUser)
+if(response.status ==200){
+    console.log(response.data);
+    localStorage.setItem('token', response.data)
+    window.location = '/'
+
+    }
+}
+        return (
+            <form className = "login" onSubmit={handlesubmit}>
+                <lable>Username</lable>
+                <input value={email} onChange={(event)=>getEmail(event.target.value)} type ='text'/>
+
+                <label>Password</label>
+                <input value={password} onChange={(event)=> getPassword(event.target.value)} type='text'/>
+                <button href="/profile" type = 'submit'> Log in </button>
+                <Button href= "/register" variant="text">Register</Button>
+            </form>
+        )
+        }
+
+        export default Login
